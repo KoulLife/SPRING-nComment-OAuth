b2389efa36d4b8150ccd1cad7ff2d553af900040
Koul <dongik0122@gmail.com>
2024-07-11 17:18:21 +0900

    first commit

---
+++ src/main/java/com/example/deepspring/DeepSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeepSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/deepspring/DeepSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
